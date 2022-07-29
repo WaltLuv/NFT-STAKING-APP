80dc6316425fc432a09a75f8a71124451ffd609a
Walter Thornton <wjt17@hotmail.com>
2022-07-29 13:46:19 -0400

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,23 @@
+import { createAlchemyWeb3 } from '@alch/alchemy-web3';
+export const NFTCONTRACT = "0x4fE7dC0D5137AEDA9efCF9275cC509BF953b237b";
+export const STAKINGCONTRACT = "0xd9E3088E21Bd3dbA065020d905B233725dab666e";
+export const polygonscanapi = "https://api-testnet.polygonscan.com/api";
+export const moralisapi = "https://deep-index.moralis.io/api/v2/";
+export const nftpng = "https://ipfs.io/ipfs/https://ipfs.io/ipfs/QmUpghC9cS1WhgoCPzTuBrC8pDjkWcvmbSXWw5GgjMxaL7/"
+export const Web3Alc = createAlchemyWeb3("https://polygon-mumbai.g.alchemy.com/v2/8AX5AP2TU6G45ctsOXNt8K4Fz_BkhhZG");
+export const NFTCONTRACT1MASTER = "0x4fE7dC0D5137AEDA9efCF9275cC509BF953b237b";
+export const STAKINGCONTRACTMASTER = "0xd9E3088E21Bd3dbA065020d905B233725dab666e";
+export const NFTCONTRACT2 = "0x5917ec1b954b88a8d16e95993d735ab040aeefac";
+export const NFTCONTRACT3RECENT = "0x055AB79025E57eCAE2e7a3fD760446eDbec90A7A";
+export const Rentals = "0x9b499C32Ee6f9DB65d610C86Fe0c290280cF5344"
+export const BMGRewards = "0x021937595614FC48cC0C60c1AF6c947A49A37B55"
+export const NFTSTAKING2 = "0xE442D0B19547B371aFF7ef8D8523C65561374eba"
+export const NFTSTAKING3 = "0x2780A149AE57516712A293ADeEd910E627eF76c5"
+export const nftpngmaster = "https://ipfs.io/ipfs/QmPhPDaRvg1m7Kw5pqc7jso5HC8EGheyThvbsHuJJTRe5o/";
+export const nftpng2 = "https://ipfs.io/ipfs/QmZD9KReYCGRFBwLP2ptNPQwMG5S2uapEvDD5nrn2MTgyo/";
+export const nftpng3 = "https://ipfs.io/ipfs/QmavM8Zpo9bD3r4zEnhbbBLLvHyfr1YL7f1faG3ovaeSSG/"
+export const nftpng4 = "https://ipfs.io/ipfs/QmUpghC9cS1WhgoCPzTuBrC8pDjkWcvmbSXWw5GgjMxaL7/"
+export const NFTCONTRACTRECENT = "0x2706A171ECb68E0038378D40Dd1d136361d0cB7d";
+export const MARKETRESELL = "0x993F00eb9C73e3E4eAe3d6Afb4Ba65A6b8B5E597";
+
+
